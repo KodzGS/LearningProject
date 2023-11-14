ac21c3c1d524df36d40718a76035a1bb1b789b54
KodzGS <k.johnson@gymshark.com>
2023-11-14 16:17:33 +0000

    Commit google test with explicit waits

--- src/test/java/stepdefs/SearchStepDefs.java
+++ src/test/java/stepdefs/SearchStepDefs.java
@@ -5,7 +5,6 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import pageObject.GoogleHomePage;
@@ -15,34 +14,32 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SearchStepDefs {
-    public WebDriver driver;
+    private WebDriver driver;
+    private GoogleHomePage googleHomePage;
 
     @Before
     public void setup() {
         driver = Setup.setupAndGetDriver();
+        googleHomePage = new GoogleHomePage(driver);
     }
     @After
     public void teardown() {
         driver.quit();
     }
     @Given("I am on the Google UK homepage")
-    public void iAmOnTheGoogleUKHomepage() throws InterruptedException {
-        GoogleHomePage.OpenGoogle(driver);
-        GoogleHomePage.CookiesSearch(driver);
-        GoogleHomePage.CookiesSearch(driver).click();
-        Thread.sleep(4000);
+    public void iAmOnTheGoogleUKHomepage() {
+        googleHomePage.openGoogle();
+        googleHomePage.acceptCookies();
     }
     @When("I enter a search term")
-    public void iEnterASearchTerm() throws InterruptedException {
-        GoogleHomePage.EnterSearchTerm(driver);
-        GoogleHomePage.EnterSearchTerm(driver).sendKeys("BBC News");
-        Thread.sleep(4000);
-        GoogleHomePage.GoogleButtonSearch(driver).click();
+    public void iEnterASearchTerm() {
+        googleHomePage.enterSearchTerm();
+        googleHomePage.googleButtonSearch();
     }
     @Then("results relevant to the search term are returned")
     public void resultsRelevantToTheSearchTermAreReturned() {
 
-        List<WebElement> resultHeaders = driver.findElements(By.xpath("//a/h3"));
+        List<WebElement> resultHeaders = googleHomePage.getSearchResults();
         for (WebElement header : resultHeaders) {
             assertThat(header.getText()).as("Search results contains search term").contains("BBC");
         }
